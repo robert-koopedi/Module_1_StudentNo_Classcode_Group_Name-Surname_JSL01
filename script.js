566746232294c86104d9b1239053fb8dbb56366b
robert koopedi <koopedi05@gmail.com>
2024-10-18 15:33:19 +0200

    Updated validateSyntax funtion

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function validateSyntax() {
-    let input = document.getElementById('petInput').value;
-    // Validation logic goes here
+    let input = document.getElementById('petInput').value;// Validation logic goes here
     let result = ''; // Placeholder for validation result
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
+        const pattern = /^pet_\d{4}[A-za-z]+$/;   //Regular expression to match pattern
 
+        if(pattern.test(input)) {
+            result = 'Valid Syntax';   // If the input matches the pattern, it is considered valid
+        }else{
+            result = 'Invalid Syntax';   // If the input  does not matches the pattern, it is considered invalid
+        }
+        
             document.getElementById('result').innerText = result;
 }
+
 
 
